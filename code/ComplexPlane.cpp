166a72fe65af22cda6dd1cfd3b16491d8b83a545
Cesar Gutierrez Raya <W2005398@apps.losrios.edu>
2023-11-18 15:55:30 -0800

    Finished draw updaterender function

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -5,26 +5,47 @@
 using namespace std;
 using namespace sf;
 
-ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
-	m_pixel_size(pixelWidth, pixelHeight);
+ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight) {
+	m_pixel_size = Vector2i(pixelWidth, pixelHeight);
 	m_aspectRatio = ((float)pixelHeight / pixelWidth);
-	m_plane_center = {0, 0};
-	m_plane_size{BASE_WIDTH, BASE_HEIGHT * m_aspectRatio};
+	m_plane_center = (Vector2f(0, 0));
+	m_plane_size = Vector2f(BASE_WIDTH, BASE_HEIGHT * m_aspectRatio);
 	m_zoomCount = 0;
-	m_State(State::CALCULATING);
+	m_state = CALCULATING;
+	m_vArray = VertexArray(Points, pixelWidth * pixelHeight);
 }
-/*
+
 void ComplexPlane::draw(RenderTarget& target, RenderStates states) const {
-
+	target.draw(m_vArray);
 }
 
-void complexPlane::updateRender() {
+void ComplexPlane::updateRender() {
+	if (m_state == CALCULATING)
+	{
+		for (int j = 0; j < pixelWidth; j++)
+		{
+			for (int i; i < pixelHeight; i++)
+			{
+				vArray[j + i * pixelWidth].position = { (float)j,(float)i };
 
+				Vector2f coord = mapPixelToCoords(j, i);
+				int iterations = countIterations(coord);
+
+				Uint8 r, g, b;
+				iterationsToRGB(iterations, r, g, b);
+
+				vArray[j + i * pixelWidth].color = { r,g,b };
+
+				m_state = DISPLAYING;
+			}
+		}
+	}
 }
-
+/**
 void ComplexPlane::zoomIn() {
 
 }
+
 
 void ComplexPlane::zoomOut() {
 
