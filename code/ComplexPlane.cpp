67404bf1508a51782f34f5532fd321f2ee7c1257
Jamba <happyjuice101@gmail.com>
2023-11-18 14:09:14 -0800

    Updated source files

--- code/ComplexPlane.cpp
+++ code/ComplexPlane.cpp
@@ -1,4 +1,7 @@
 #include "ComplexPlane.h"
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+using namespace sf;
 using namespace std;
 
 ComplexPlane::ComplexPlane(int pixelWidth, int pixelHeight){
