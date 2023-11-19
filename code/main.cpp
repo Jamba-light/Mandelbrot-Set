6508c1d5a25958afa12341a1f0ae8c243da2e941
Jamba <happyjuice101@gmail.com>
2023-11-18 18:31:23 -0800

    Main updated

--- code/main.cpp
+++ code/main.cpp
@@ -16,7 +16,9 @@ int main()
     unsigned int height = VideoMode::getDesktopMode().height / 2;
     VideoMode vm(width, height);
     RenderWindow window(vm, "Mandelbrot Set", Style::Default);
+
     ComplexPlane plane(width,height);
+
     sf::Font font;
     if (!font.loadFromFile("coolvetica.ttf"))
         return(-1);
@@ -43,25 +45,37 @@ int main()
         Event event;
         while (window.pollEvent(event))
         {
+            if (event.type == Event::Closed)
+            {
+                // Quit the game when the window is closed
+                window.close();
+            }
+
+            //Zoom out
+            if (event.mouseButton.button == Mouse::Right)
+            {
+                Vector2i mousePosition = Vector2i(event.mouseButton.x, event.mouseButton.x);
+                plane.zoomOut();
+                plane.setCenter(mousePosition);
+            }
+
+            //Zoom in
+            if (event.mouseButton.button == Mouse::Left)
+            {
+                Vector2i mousePosition = Vector2i(event.mouseButton.x, event.mouseButton.x);
+                plane.zoomIn();
+                plane.setCenter(mousePosition);
+            }
+
+            if (event.MouseMoved)
+            {
+                //plane.setMouseLocation(Mouse getPosition(window);
+            }
+
             if (Keyboard::isKeyPressed(Keyboard::Escape))
             {
                 window.close();
             }
-            if (event.mouseButton.button == Mouse::Right)
-            {
-                Vector2i mousePosition = Mouse::getPosition(window);
-                plane.zoomOut();
-                plane.setCenter(mousePosition);
-
-               
-            }
-            if (Keyboard::isKeyPressed(Keyboard::Left))
-            {
-                Vector2i mousePosition = Mouse::getPosition(window);
-                plane.zoomIn();
-                plane.setCenter(mousePosition);
-            }
-          
         }
 
         plane.updateRender();
