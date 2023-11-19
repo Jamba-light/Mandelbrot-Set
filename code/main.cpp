0d14cc8022009a5d58bad11e7ffbd4ac3cd77b9e
cesaregutierrez2004 <150281984+cesaregutierrez2004@users.noreply.github.com>
2023-11-18 17:50:48 -0800

    Update main.cpp

--- code/main.cpp
+++ code/main.cpp
@@ -31,18 +31,42 @@ int main()
         Prompt the user
         ****************************************
         */
-        /*
+        
         firstText.setFont(font);
-        firstText.setString("");
-        firstText.setCharacterSize();
-        firstText.setFillColor(sf::Color::);
-        firstText.setPosition(,);
+        firstText.setCharacterSize(50);
+        firstText.setFillColor(sf::Color::White);
+        firstText.setPosition(0,0);
+        ComplexPlane.loadText(firstText);
+        
+        Event event;
+        while (window.pollEvent(event))
+        {
+            if (Keyboard::isKeyPressed(Keyboard::Escape))
+            {
+                window.close();
+            }
+            if (event.mouseButton.button == Mouse::Right)
+            {
+                Vector2i mousePosition = Mouse::getPosition(window);
+                ComplexPlane.zoomOut();
+                ComplexPlane.setCenter(mousePosition);
+                ComplexPlane.setState(CALCULATING);
+            }
+            if (Keyboard::isKeyPressed(Keyboard::Left))
+            {
+                Vector2i mousePosition = Mouse::getPosition(window);
+                ComplexPlane.zoomIn();
+                ComplexPlane.setCenter(mousePosition);
+                ComplexPlane.setState(CALCULATING);
+            }
+          
 
-        secondText.setFont(font);
-        secondText.setString("");
-        secondText.setCharacterSize();
-        secondText.setFillColor(sf::Color::);
-        secondText.setPosition(,);
-        */
+
+
+
+
+        }
+
+       
     }
 }
